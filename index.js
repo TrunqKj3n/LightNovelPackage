1cea42a9d168040e7fb3473a0d78969f5f039079
Kien <ttk.trungkien333@gmail.com>
2022-03-27 03:17:54 +0000

    ln

---
+++ index.js
@@ -0,0 +1,10 @@
+const { searchLN, getChapterLN, getObjLN } = require('./lib/function')
+
+module.exports.searchLN = searchLN
+module.exports.getChapterLN = getChapterLN
+module.exports.getObjLN = getObjLN
+
+
+
+
+
